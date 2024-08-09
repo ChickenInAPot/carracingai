74df2eb3fd0a48d9b929e9d614c1303881f2443c
ChickenInAPot <derespa06@gmail.com>
2024-08-08 20:38:27 -0600

    pygame set up

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame, sys
+
+width, height = 500, 500
+screen = pygame.display.set_mode((width, height))
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    
+
+
+pygame.quit()
+sys.exit()
