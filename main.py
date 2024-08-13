3e0ba8f2ed65d8f8be67637cbb4136d7f8b3ef6d
ChickenInAPot <derespa06@gmail.com>
2024-08-13 13:41:44 -0600

    yeah idk bruh

--- main.py
+++ main.py
@@ -1,21 +1,57 @@
 import pygame, sys, time, math
-from utils import  scale_image 
+from utils import  scale_image, blit_rotate_center
 
 width, height = 500, 500
 screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("👨🏾‍🦲")
+
 
 fps = 60
 running = True
 
-car = scale_image(pygame.image.load("/imgs/white-car.png"))
+grass = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
+track = scale_image(pygame.image.load("imgs/track.png"), 0.9)
+car = scale_image(pygame.image.load("imgs/white-car.png"), 0.5)
 
+startpos = (180,200)
 
-class car():
-    def movement:
+class Car():
+    def __init__(self, maxvel, rotvel):
+        self.img = car
+        self.max_vel = maxvel
+        self.vel = 0
+        self.rotation_vel = rotvel
+        self.angle = 0
+        self.x, self.y = startpos
+        self.acceleration = 0.1
+    def rotate(self, left = False, right = False):
+        if left:
+            self.angle += self.rotation_vel
+        elif right:
+            self.angle -= self.rotation_vel
+    def draw(self, screen):
+        blit_rotate_center(screen, self.img, (self.x, self.y), self.angle)
+    def move_forward(self):
+        
+
+    
+
+def draw(screen, images, Car):
+    for img, pos in images:
+        screen.blit(img, pos)
+
+    Car.draw(screen)
+    pygame.display.update()
+
+images = [(track, (0, 0)), (grass, (0, 0))]
+test = Car(4,4)
 
 while running:
+
     pygame.time.Clock().tick(fps)
     
+    draw(screen, images, test)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
--- utils.py
+++ utils.py
@@ -4,5 +4,8 @@ def scale_image(img, scale):
     size = round(img.get_width() * scale), round(img.get_height() * scale)
     return pygame.transform.scale(img, size)
 
-def rotate_center(win, image, top_left, angle):
-    
+def blit_rotate_center(win, image, top_left, angle):
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(
+    center=image.get_rect(topleft=top_left).center)
+    win.blit(rotated_image, new_rect.topleft)
